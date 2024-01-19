dccebc089032757fb4c87b5ecf0397f755c616b0
soogoori <soobin0209@naver.com>
2024-01-19 11:41:34 +0900

    feat: 무한스크롤 구현 중

--- showmore.js
+++ showmore.js
@@ -1,8 +1,28 @@
 const imageList = document.querySelector(".img-container");
+const showMore = document.querySelector(".img-wrap");
+const moreImgs = document.querySelector(".img-wrap:not(:nth-child(n+3))")
+const showMoreBtn = document.querySelector(".btn-showmore");
 let pageToFetch = 1;
+
+// throttling
+const throttling = (callback, delay) => {
+    let timer = null;
+    return () => {
+        console.log(timer);
+
+        if (timer === null) {
+            timer = setTimeout(() => {
+                callback();
+                timer = null;
+            }, delay);
+        }
+    };
+};
+
+// 이미지 불러오기
 async function fetchImages(pageNum){
     try {
-        const response = await fetch('https://picsum.photos/v2/list?page='+pageNum+'&limit=10');
+        const response = await fetch('https://picsum.photos/v2/list?page='+pageNum+'&limit=6');
         if (!response.ok) {
             throw new Error("네트워크 응답에 문제가 있습니다.");
         }
@@ -16,12 +36,25 @@ async function fetchImages(pageNum){
 }
 
 function makeImageList(datas){
-    datas.forEach((item)=>{
-        imageList.innerHTML += "<div class='img-wrap'><img src="+ item.download_url +" alt=''></div>";
-    })
+    for (let i = 0; i < datas.length; i+=3) {
+        imageList.innerHTML += "<div class='img-wrap'>" +
+            "<img src="+ datas[i].download_url +" alt=''>" +
+            "<img src="+ datas[i+1].download_url +" alt=''>" +
+            "<img src="+ datas[i+2].download_url +" alt=''></div>";
+    }
 }
 
-window.addEventListener('scroll', () => {
+function showMoreImgs() {
+    moreImgs.style.display = "flex";
+    window.addEventListener('scroll', throttling(infinityScroll, 2000));
+}
+
+showMoreBtn.addEventListener('click', showMoreImgs);
+
+
+// 무한 스크롤
+function infinityScroll() {
+
     // 스크롤이 상단으로부터 얼마나 이동했는지 알아야함. (viewport 높이 + scroll된 길이)
     // 화면에 로딩된 페이지의 전체 높이 === 뷰포트 높이 + 스크롤된 길이 + 5~10px(매끄럽게 하기 위해서)
 
@@ -29,5 +62,15 @@ window.addEventListener('scroll', () => {
         document.documentElement.offsetHeight){
         fetchImages(pageToFetch++);
     }
-});
+}
 fetchImages();
+
+
+// 상단으로 올라가는 버튼
+const goUpBtn = document.querySelector("#arrow-hover");
+
+goUpBtn.addEventListener("click",()=>{
+
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+})
+
